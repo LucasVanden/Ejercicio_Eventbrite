9da672c3aad55dfa7777c19ecd9c76bf91d144a2
Van den <vandenboschlucas@hotmail.com>
2019-05-08 19:20:48 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio {
+    class RandomNumber {
+        Random r = new Random();
+        int x = 0;
+        int q = 0;
+        int count = 0;
+        string generado;
+        public string GenerateRandomNumber() {
+
+            while (true) {
+                x = r.Next(1000, 9999);
+                generado = x.ToString();
+                q = 0;
+                count = 0;
+
+                for (q = 0; q < generado.Length; q++) {
+                    for (int w = 0; w < generado.Length; w++) {
+                        if (generado[w] == generado[q]) {
+                            count++;
+                        }
+                    }
+                }
+                if (count == 4) {
+                    return generado;
+                }
+
+            }
+        }
+    }
+}
---
+++ Ejercicio/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Ejercicio {
+    class Program {
+   
+        static void Main(string[] args) {
+            RandomNumber a = new RandomNumber();
+            string generado = a.GenerateRandomNumber();
+
+  //            Console.WriteLine(generado);
+
+            int aciertos = 0;
+            int reguar = 0;
+
+//mientras no gane seguir preguntando
+            while (aciertos != 4) {
+                Console.WriteLine("adivinar numero");
+                string numeroIntentodeAdivinar;
+
+ //Limitar a solo 4 digitos
+                while (true) {
+                    numeroIntentodeAdivinar = Console.ReadLine();
+                    if (numeroIntentodeAdivinar.Length != 4) {
+                        Console.WriteLine("ingrese numero de 4 digitos");
+                    }
+                    else break;
+                }
+
+//Calcular aciertos
+                for (int i = 0; i < generado.Length; i++) {
+                    if (generado[i] == numeroIntentodeAdivinar[i]) {
+                        aciertos++;
+                    }
+                }
+
+//Calcular regulares
+                for (int j = 0; j < generado.Length; j++) {
+
+                    if (generado.Contains(numeroIntentodeAdivinar[j])) {
+                        reguar++;
+                    }
+                }
+                reguar = reguar - aciertos;
+
+//Mostrar en pantalla
+                Console.WriteLine($"aciertos '{aciertos}' regular {reguar} ");
+                if (aciertos == 4) {
+                    Console.WriteLine("WIN");
+                    Console.Read();
+                }
+ //Reset           
+                else { aciertos = 0; reguar = 0; }
+
+            }
+
+        }
+
+    }
+}
