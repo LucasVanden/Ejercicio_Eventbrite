070d0523007c748a6f8531debda4f9a0d9deb20d
Van den <vandenboschlucas@hotmail.com>
2019-05-09 19:58:08 -0300

    Creacion de clase Comprobador,Inicio Parte 2

---
+++ Ejercicio/Comprobador.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio {
+    class Comprobador {
+        public int comprobarResulatado(string generado, string numeroIntentodeAdivinar) {
+            int aciertos = 0;
+            int regular = 0;
+
+            //Calcular aciertos
+            for (int i = 0; i < generado.Length; i++) {
+                if (generado[i] == numeroIntentodeAdivinar[i]) {
+                    aciertos++;
+                }
+            }
+
+            //Calcular regulares
+            for (int j = 0; j < generado.Length; j++) {
+
+                if (generado.Contains(numeroIntentodeAdivinar[j])) {
+                    regular++;
+                }
+            }
+            regular = regular - aciertos;
+
+            //Mostrar en pantalla
+            Console.WriteLine($"aciertos '{aciertos}' regular {regular} ");
+            if (aciertos == 4) {
+                Console.WriteLine("WIN");
+                Console.Read();
+            }
+            //Reset           
+            return aciertos;
+        }
+
+            
+        }
+    }
+
--- Ejercicio/Program.cs
+++ Ejercicio/Program.cs
@@ -4,15 +4,17 @@ namespace Ejercicio {
     class Program {
    
         static void Main(string[] args) {
+
+//Parte 1
             RandomNumber a = new RandomNumber();
             string generado = a.GenerateRandomNumber();
+            Comprobador comprobador = new Comprobador();
+            Console.WriteLine(generado);
 
-  //            Console.WriteLine(generado);
 
-            int aciertos = 0;
-            int reguar = 0;
 
 //mientras no gane seguir preguntando
+            int aciertos = 0;
             while (aciertos != 4) {
                 Console.WriteLine("adivinar numero");
                 string numeroIntentodeAdivinar;
@@ -26,34 +28,124 @@ namespace Ejercicio {
                     else break;
                 }
 
-//Calcular aciertos
-                for (int i = 0; i < generado.Length; i++) {
-                    if (generado[i] == numeroIntentodeAdivinar[i]) {
-                        aciertos++;
-                    }
-                }
-
-//Calcular regulares
-                for (int j = 0; j < generado.Length; j++) {
-
-                    if (generado.Contains(numeroIntentodeAdivinar[j])) {
-                        reguar++;
-                    }
-                }
-                reguar = reguar - aciertos;
-
-//Mostrar en pantalla
-                Console.WriteLine($"aciertos '{aciertos}' regular {reguar} ");
-                if (aciertos == 4) {
-                    Console.WriteLine("WIN");
-                    Console.Read();
-                }
- //Reset           
-                else { aciertos = 0; reguar = 0; }
+              
+             aciertos = comprobador.comprobarResulatado(generado,numeroIntentodeAdivinar);
 
             }
+ //Parte 2
 
-        }
+//Leer numero
+            String respuestaCorrecta;
+            String test="";
+            aciertos=0;
+            int posicion = 0;
+            String[] vec = new string[4];
+            String[] vec2 = new string[4];
 
-    }
+            while (true) {
+                respuestaCorrecta = Console.ReadLine();
+                if (respuestaCorrecta.Length != 4) {
+                    Console.WriteLine("inserta un numero de 4 cifras distintas");
+                }
+                else break;
+            }
+//calcular cifras acertadas
+            for (int c = 0; c < 10; c++) {
+                test = c + "" + c + "" + c + "" + c;
+                Console.WriteLine(test);
+
+               
+               aciertos = comprobador.comprobarResulatado(respuestaCorrecta, test);
+                
+                if (aciertos == 1) {
+                    vec[posicion] = c.ToString();
+                    posicion++;   
+                }
+                if (posicion==4) 
+                    break;
+                
+            }
+//probar combinaciones
+            String testOriginal = vec[0] + "" + vec[1] + "" + vec[2] + "" + vec[3];
+          //  int acietosAnterior = 0;
+            Console.WriteLine(testOriginal);
+            aciertos = comprobador.comprobarResulatado(respuestaCorrecta, testOriginal);
+
+            if (aciertos==4) {
+                Console.Read();
+            }
+
+//rotar mientras aciertos sean 0
+            vec.CopyTo(vec2, 0);
+            while (aciertos==0) {
+                String temp = vec[0];
+                String temp1 = vec[1];
+                vec[0]= vec[3];
+                vec[1]= temp;
+                temp = vec[2];
+                vec[2]= temp1;
+                vec[3] = temp;
+
+                test = vec[0] + "" + vec[1] + "" + vec[2] + "" + vec[3];
+                Console.WriteLine(test);
+                aciertos = comprobador.comprobarResulatado(respuestaCorrecta, test);
+                vec.CopyTo(vec2,0) ;
+            }
+            if (aciertos == 2) {
+//intercambio 0-1
+                String temp = vec[0];
+                vec[0] = vec[1];
+                vec[1] = temp;
+                test = vec[0] + "" + vec[1] + "" + vec[2] + "" + vec[3];
+                Console.WriteLine(test);
+                aciertos = comprobador.comprobarResulatado(respuestaCorrecta, test);
+                if (aciertos == 0) {
+                    vec2.CopyTo(vec, 0);
+                    
+                    temp = vec[3];
+                    vec[3] = vec[2];
+                    vec[2] = temp;
+                    test = vec[0] + "" + vec[1] + "" + vec[2] + "" + vec[3];
+                    Console.WriteLine(test);
+                    aciertos = comprobador.comprobarResulatado(respuestaCorrecta, test);
+
+                }
+//intercambio 1-2
+                vec2.CopyTo(vec, 0);
+                temp = vec[1];
+                vec[1] = vec[2];
+                vec[2] = temp;
+                test = vec[0] + "" + vec[1] + "" + vec[2] + "" + vec[3];
+                Console.WriteLine(test);
+                aciertos = comprobador.comprobarResulatado(respuestaCorrecta, test);
+                if (aciertos == 0) {
+                    vec2.CopyTo(vec, 0);
+                    temp = vec[0];
+                    vec[0] = vec[3];
+                    vec[3] = temp;
+                    test = vec[0] + "" + vec[1] + "" + vec[2] + "" + vec[3];
+                    Console.WriteLine(test);
+                    aciertos = comprobador.comprobarResulatado(respuestaCorrecta, test);
+                }
+//intercambio 1-3 o 0-2
+                vec2.CopyTo(vec, 0);
+                temp = vec[1];
+                vec[1] = vec[3];
+                vec[3] = temp;
+                test = vec[0] + "" + vec[1] + "" + vec[2] + "" + vec[3];
+                Console.WriteLine(test);
+                aciertos = comprobador.comprobarResulatado(respuestaCorrecta, test);
+                if (aciertos == 0) {
+                    vec2.CopyTo(vec, 0);
+                    temp = vec[0];
+                    vec[0] = vec[2];
+                    vec[2] = temp;
+                    test = vec[0] + "" + vec[1] + "" + vec[2] + "" + vec[3];
+                    Console.WriteLine(test);
+                    aciertos = comprobador.comprobarResulatado(respuestaCorrecta, test);
+                }
+            }
+                
+              
+        }    }
 }
